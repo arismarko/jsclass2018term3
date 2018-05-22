9142563f3c3df5a777ec9045a9884038da3eccaf
Pawel Klasa <pawel.klasa@gmail.com>
2018-05-22 10:32:06 +0100

    innit

---
+++ app.js
@@ -0,0 +1,101 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-modal")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Reminder constructor
+function Reminder(reminderTitle, reminderDate, reminderPriority, reminderText) {
+  this.reminderTitle = reminderTitle;
+  this.reminderDate = reminderDate;
+  this.reminderPriority = reminderPriority;
+  this.reminderText = reminderText;
+}
+
+document.getElementById('btn').addEventListener('click', function(e) {
+  const reminderTitle = document.getElementById('title').value,
+        reminderDate = document.getElementById('date').value,
+        reminderPriority = document.getElementById('priority').value,
+        reminderText = document.getElementById('text').value;
+
+  // Getting all elements in item-holder
+  const itemHolder = document.getElementById('item-holder'),
+        itemCard = document.createElement('div'),
+        title = document.createElement('h4'),
+        text = document.createElement('p'),
+        date = document.createElement('p'),
+        close = document.createElement('i'),
+        edit = document.createElement('i'),
+        priority = document.createElement('i');
+
+  // Creating each item in a card
+  itemHolder.appendChild(itemCard);
+  itemCard.className = 'item card-item';
+
+  itemCard.appendChild(title);
+  title.innerHTML = reminderTitle;
+
+  itemCard.appendChild(close);
+  close.className = 'far fa-times-circle fa-sm close';
+  close.id = "close";
+
+  itemCard.appendChild(edit);
+  edit.className = 'far fa-edit fa-sm edit';
+
+  itemCard.appendChild(priority);
+  priority.className = 'fas fa-flag fa-sm priority';
+
+  itemCard.appendChild(date);
+  date.className = 'no-p-margin date';
+  date.innerHTML = reminderDate;
+
+  itemCard.appendChild(text);
+  text.innerHTML = reminderText;
+
+  // Priority colour change
+  if (reminderPriority === 'P1') {
+  priority.style = 'color: #e6496b';
+  }
+  if (reminderPriority === 'P2') {
+    priority.style = 'color: #f46c22'
+  }
+  if (reminderPriority === 'P3') {
+    priority.style = 'color: #fccc98'
+  }
+
+  // Close
+  document.getElementById('close').addEventListener('click', function() {
+    itemHolder.removeChild(itemCard);
+    console.log('clicked');
+  });
+
+  // Remove modal
+  modal.style.display = "none";
+  // Make item appear
+  // itemHolder.style.display = "block";
+
+  e.preventDefault();
+
+});
+
+
