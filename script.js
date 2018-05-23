46a6a4a22cfd5e4ec89dfb4575d6fd1c97fb283a
JoeClarkMcGhee <joeclarkmcghee@yahoo.co.uk>
2018-05-23 23:49:47 +0100

    add date to list on form submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const fieldIDs = ["taskFormEntry","completedDateFormEntry","categoryFormEntry"];
-const listIDs = ["toDo-item","toDo-completeBy","toDo-category"];
+const fieldIDs = ["taskFormEntry", "completedDateFormEntry", "categoryFormEntry"];
+const listIDs = ["toDo-item", "toDo-completeBy", "toDo-category"];
 const form = document.getElementById('form');
 
 // get values from form
@@ -9,16 +9,32 @@ getFormEntry = field => {
 };
 
 // function to append to list
-appendFormEntryToList = (field, listClassName) => {
+appendFormEntryToList = (field, listIDName) => {
     let newLIElement = document.createElement("li");
     let listItemValue = document.createTextNode(getFormEntry(field));
     newLIElement.appendChild(listItemValue);
-    document.getElementById(listClassName).appendChild(newLIElement);
+    document.getElementById(listIDName).appendChild(newLIElement);
 };
+
+//add current time to list following form submission
+addDateToList = () => {
+    let today = new Date().toISOString().slice(0, 10)
+    let newLIElement = document.createElement("li");
+    let listItemValue = document.createTextNode(today);
+    newLIElement.appendChild(listItemValue);
+    document.getElementById("toDo-dateAdded").appendChild(newLIElement);
+};
+
+// add an edit button
+
+// add a remove button - make this populate a separate remove tab
+
+
 
 // Add form entries after submit
 form.addEventListener("submit", (event) => {
-
+    // add date added
+    addDateToList()
     // add task to document
     appendFormEntryToList(fieldIDs[0], listIDs[0]);
     // add completed by date to document
